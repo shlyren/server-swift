7415667b3efae2446e086a07466a5ba26833c790
任玉祥 <mail@yuxiang.ren>
2018-03-15 17:30:42 +0800

    update: 2018-03-15 17:30:42

--- Package.swift
+++ Package.swift
@@ -20,13 +20,13 @@
 import PackageDescription
 
 let package = Package(
-	name: "CTServer",
+	name: "CTServer", // 项目名称, 即编译后的二进制文件名称
 	targets: [],
 	dependencies: [
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 2),
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-MySQL.git", majorVersion: 3),
-        .Package(url:"https://github.com/PerfectlySoft/Perfect-Notifications.git", majorVersion: 2, minor: 0),
-        .Package(url:"https://github.com/PerfectlySoft/Perfect-WebSockets.git", majorVersion: 2),
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 2), // http 服务
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-MySQL.git", majorVersion: 3), // 数据库
+        .Package(url:"https://github.com/PerfectlySoft/Perfect-Notifications.git", // iOS推送 majorVersion: 2, minor: 0),
+        .Package(url:"https://github.com/PerfectlySoft/Perfect-WebSockets.git", majorVersion: 2), // 及时通讯
     ]
 )
 
